3f7de8ca294d61520a7054fa3ead915434a09f31
Daniel Lea <dan.carr.lea@gmail.com>
2024-10-21 13:57:01 +0100

    add LSTM lightning module (#4)

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import pytorch_lightning as pl
 
 
 class NeuralNetwork(nn.Module):
@@ -28,3 +29,32 @@ class NeuralNetwork(nn.Module):
         output = self.fc(lstm_out[:, -1, :])
 
         return torch.log_softmax(output, dim=1)
+
+
+# LightningModule for training the LSTM model
+class LSTMModel(pl.LightningModule):
+    def __init__(
+        self, vocab_size: int, embedding_dim: int, hidden_dim: int, output_dim: int
+    ):
+        super().__init__()
+        self.model = NeuralNetwork(vocab_size, embedding_dim, hidden_dim, output_dim)
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        inputs, targets = batch
+        logits = self(inputs)
+        loss = self.loss_fn(logits, targets[:, -1])
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        inputs, targets = batch
+        logits = self(inputs)
+        loss = self.loss_fn(logits, targets[:, -1])
+        self.log("val_loss", loss)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
